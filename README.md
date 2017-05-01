826f7adf60b596f591929a32b10f1fb720355e2a
Mathieu Auclair <AucMa1633485@cegepjonquiere.ca>
2017-05-01 13:59:29 -0400

    adding graphical interface

--- README.md
+++ README.md
@@ -4,9 +4,17 @@ this is a uber simple project I'm making for one of my class as a final project,
 
 it's an online multiplayer blackjack game
 
+### dependencies
+
+* nodejs
+
+* npm (usually include with nodejs)
+
 ### how to use it?
 
 1- open a terminal in the project folder
+
+2- enter the command "npm install"
 
 2- enter the command "node server.js"
 
---
+++ WindowsFormApp/LabBlackjack/frmJeu.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace LabBlackjack
+{
+    partial class frmJeu
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmJeu));
+            this.picFinCarte = new System.Windows.Forms.PictureBox();
+            this.picDemanderCarte = new System.Windows.Forms.PictureBox();
+            this.picDistribuerCarte = new System.Windows.Forms.PictureBox();
+            this.picJ5 = new System.Windows.Forms.PictureBox();
+            this.picJ4 = new System.Windows.Forms.PictureBox();
+            this.picJ3 = new System.Windows.Forms.PictureBox();
+            this.picJ2 = new System.Windows.Forms.PictureBox();
+            this.picJ1 = new System.Windows.Forms.PictureBox();
+            this.picC5 = new System.Windows.Forms.PictureBox();
+            this.picC4 = new System.Windows.Forms.PictureBox();
+            this.picC3 = new System.Windows.Forms.PictureBox();
+            this.picC2 = new System.Windows.Forms.PictureBox();
+            this.picC1 = new System.Windows.Forms.PictureBox();
+            this.lblCptC = new System.Windows.Forms.Label();
+            this.lblCptJ = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picFinCarte)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDemanderCarte)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDistribuerCarte)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJ5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJ4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJ3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJ2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJ1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picFinCarte
+            // 
+            this.picFinCarte.BackColor = System.Drawing.Color.Transparent;
+            this.picFinCarte.Location = new System.Drawing.Point(519, 319);
+            this.picFinCarte.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picFinCarte.Name = "picFinCarte";
+            this.picFinCarte.Size = new System.Drawing.Size(40, 46);
+            this.picFinCarte.TabIndex = 71;
+            this.picFinCarte.TabStop = false;
+            // 
+            // picDemanderCarte
+            // 
+            this.picDemanderCarte.BackColor = System.Drawing.Color.Transparent;
+            this.picDemanderCarte.Location = new System.Drawing.Point(477, 319);
+            this.picDemanderCarte.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picDemanderCarte.Name = "picDemanderCarte";
+            this.picDemanderCarte.Size = new System.Drawing.Size(38, 46);
+            this.picDemanderCarte.TabIndex = 70;
+            this.picDemanderCarte.TabStop = false;
+            // 
+            // picDistribuerCarte
+            // 
+            this.picDistribuerCarte.BackColor = System.Drawing.Color.Transparent;
+            this.picDistribuerCarte.Location = new System.Drawing.Point(434, 319);
+            this.picDistribuerCarte.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picDistribuerCarte.Name = "picDistribuerCarte";
+            this.picDistribuerCarte.Size = new System.Drawing.Size(38, 46);
+            this.picDistribuerCarte.TabIndex = 69;
+            this.picDistribuerCarte.TabStop = false;
+            // 
+            // picJ5
+            // 
+            this.picJ5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picJ5.BackgroundImage")));
+            this.picJ5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picJ5.Location = new System.Drawing.Point(304, 255);
+            this.picJ5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picJ5.Name = "picJ5";
+            this.picJ5.Size = new System.Drawing.Size(56, 73);
+            this.picJ5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picJ5.TabIndex = 67;
+            this.picJ5.TabStop = false;
+            this.picJ5.Visible = false;
+            // 
+            // picJ4
+            // 
+            this.picJ4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picJ4.BackgroundImage")));
+            this.picJ4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picJ4.Location = new System.Drawing.Point(274, 255);
+            this.picJ4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picJ4.Name = "picJ4";
+            this.picJ4.Size = new System.Drawing.Size(56, 73);
+            this.picJ4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picJ4.TabIndex = 68;
+            this.picJ4.TabStop = false;
+            this.picJ4.Visible = false;
+            // 
+            // picJ3
+            // 
+            this.picJ3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picJ3.Image = ((System.Drawing.Image)(resources.GetObject("picJ3.Image")));
+            this.picJ3.Location = new System.Drawing.Point(250, 255);
+            this.picJ3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picJ3.Name = "picJ3";
+            this.picJ3.Size = new System.Drawing.Size(56, 73);
+            this.picJ3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picJ3.TabIndex = 66;
+            this.picJ3.TabStop = false;
+            this.picJ3.Visible = false;
+            // 
+            // picJ2
+            // 
+            this.picJ2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picJ2.BackgroundImage")));
+            this.picJ2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picJ2.Location = new System.Drawing.Point(226, 255);
+            this.picJ2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picJ2.Name = "picJ2";
+            this.picJ2.Size = new System.Drawing.Size(56, 73);
+            this.picJ2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picJ2.TabIndex = 65;
+            this.picJ2.TabStop = false;
+            // 
+            // picJ1
+            // 
+            this.picJ1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picJ1.BackgroundImage")));
+            this.picJ1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picJ1.Location = new System.Drawing.Point(200, 255);
+            this.picJ1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picJ1.Name = "picJ1";
+            this.picJ1.Size = new System.Drawing.Size(56, 73);
+            this.picJ1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picJ1.TabIndex = 64;
+            this.picJ1.TabStop = false;
+            // 
+            // picC5
+            // 
+            this.picC5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picC5.BackgroundImage")));
+            this.picC5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picC5.Location = new System.Drawing.Point(413, 112);
+            this.picC5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picC5.Name = "picC5";
+            this.picC5.Size = new System.Drawing.Size(54, 73);
+            this.picC5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picC5.TabIndex = 63;
+            this.picC5.TabStop = false;
+            this.picC5.Visible = false;
+            // 
+            // picC4
+            // 
+            this.picC4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picC4.BackgroundImage")));
+            this.picC4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picC4.Location = new System.Drawing.Point(386, 112);
+            this.picC4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picC4.Name = "picC4";
+            this.picC4.Size = new System.Drawing.Size(54, 73);
+            this.picC4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picC4.TabIndex = 62;
+            this.picC4.TabStop = false;
+            this.picC4.Visible = false;
+            // 
+            // picC3
+            // 
+            this.picC3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picC3.BackgroundImage")));
+            this.picC3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picC3.Location = new System.Drawing.Point(359, 112);
+            this.picC3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picC3.Name = "picC3";
+            this.picC3.Size = new System.Drawing.Size(56, 73);
+            this.picC3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picC3.TabIndex = 61;
+            this.picC3.TabStop = false;
+            this.picC3.Visible = false;
+            // 
+            // picC2
+            // 
+            this.picC2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picC2.BackgroundImage")));
+            this.picC2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picC2.Location = new System.Drawing.Point(334, 112);
+            this.picC2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picC2.Name = "picC2";
+            this.picC2.Size = new System.Drawing.Size(54, 73);
+            this.picC2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picC2.TabIndex = 60;
+            this.picC2.TabStop = false;
+            // 
+            // picC1
+            // 
+            this.picC1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picC1.BackgroundImage")));
+            this.picC1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picC1.Location = new System.Drawing.Point(313, 112);
+            this.picC1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picC1.Name = "picC1";
+            this.picC1.Size = new System.Drawing.Size(54, 73);
+            this.picC1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picC1.TabIndex = 59;
+            this.picC1.TabStop = false;
+            // 
+            // lblCptC
+            // 
+            this.lblCptC.AutoSize = true;
+            this.lblCptC.BackColor = System.Drawing.Color.Transparent;
+            this.lblCptC.ForeColor = System.Drawing.Color.Yellow;
+            this.lblCptC.Location = new System.Drawing.Point(367, 197);
+            this.lblCptC.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCptC.Name = "lblCptC";
+            this.lblCptC.Size = new System.Drawing.Size(0, 13);
+            this.lblCptC.TabIndex = 72;
+            // 
+            // lblCptJ
+            // 
+            this.lblCptJ.AutoSize = true;
+            this.lblCptJ.BackColor = System.Drawing.Color.Transparent;
+            this.lblCptJ.ForeColor = System.Drawing.Color.Yellow;
+            this.lblCptJ.Location = new System.Drawing.Point(256, 341);
+            this.lblCptJ.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCptJ.Name = "lblCptJ";
+            this.lblCptJ.Size = new System.Drawing.Size(0, 13);
+            this.lblCptJ.TabIndex = 73;
+            // 
+            // frmJeu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(654, 467);
+            this.Controls.Add(this.lblCptJ);
+            this.Controls.Add(this.lblCptC);
+            this.Controls.Add(this.picFinCarte);
+            this.Controls.Add(this.picDemanderCarte);
+            this.Controls.Add(this.picDistribuerCarte);
+            this.Controls.Add(this.picJ5);
+            this.Controls.Add(this.picJ4);
+            this.Controls.Add(this.picJ3);
+            this.Controls.Add(this.picJ2);
+            this.Controls.Add(this.picJ1);
+            this.Controls.Add(this.picC5);
+            this.Controls.Add(this.picC4);
+            this.Controls.Add(this.picC3);
+            this.Controls.Add(this.picC2);
+            this.Controls.Add(this.picC1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmJeu";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picFinCarte)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDemanderCarte)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDistribuerCarte)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJ5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJ4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJ3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJ2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picJ1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picFinCarte;
+        private System.Windows.Forms.PictureBox picDemanderCarte;
+        private System.Windows.Forms.PictureBox picDistribuerCarte;
+        private System.Windows.Forms.PictureBox picJ5;
+        private System.Windows.Forms.PictureBox picJ4;
+        private System.Windows.Forms.PictureBox picJ3;
+        private System.Windows.Forms.PictureBox picJ2;
+        private System.Windows.Forms.PictureBox picJ1;
+        private System.Windows.Forms.PictureBox picC5;
+        private System.Windows.Forms.PictureBox picC4;
+        private System.Windows.Forms.PictureBox picC3;
+        private System.Windows.Forms.PictureBox picC2;
+        private System.Windows.Forms.PictureBox picC1;
+        private System.Windows.Forms.Label lblCptC;
+        private System.Windows.Forms.Label lblCptJ;
+    }
+}
+
